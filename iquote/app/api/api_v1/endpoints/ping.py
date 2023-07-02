7107b0f4e234f510bbea0523c6cc14436a92c282
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2023-07-02 17:19:35 +0800

    dev feat: add new test for ping

---
+++ iquote/app/api/api_v1/endpoints/ping.py
@@ -0,0 +1,16 @@
+# from project/app/api/ping.py
+
+from fastapi import APIRouter, Depends
+
+from app.core.config import Settings, get_settings
+
+router = APIRouter()
+
+
+@router.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.ENVIRONMENT,
+        "testing": settings.TESTING
+    }
---
+++ iquote/app/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from app.api.api_v1.api import api_router
+from app.core.config import settings
+import logging
+
+log = logging.getLogger("uvicorn")
+
+
+def create_application() -> FastAPI:
+    application = FastAPI(
+        title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
+    )
+    # Set all CORS enabled origins
+    if settings.BACKEND_CORS_ORIGINS:
+        application.add_middleware(
+            CORSMiddleware,
+            allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+        
+    application.include_router(api_router, prefix=settings.API_V1_STR)
+   
+    return application
+
+
+app = create_application()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    # init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
