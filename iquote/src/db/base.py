ed3654b329a3d15b51704c7d362a0a2bc71656b1
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2023-07-13 02:10:00 +0800

    dev fix: fixing fresh install db to ready with make run-test

--- iquote/src/db/base.py
+++ iquote/src/db/base.py
@@ -1,5 +1,4 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
 from db.base_class import Base  # noqa
-from models.item import Item  # noqa
-from models.user import User  # noqa
+from models.user import User, likes  # noqa
--- iquote/tests/api/api_v1/test_categories.py
+++ iquote/tests/api/api_v1/test_categories.py
@@ -1,5 +1,3 @@
-from typing import Dict
-
 import faker
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
@@ -7,68 +5,6 @@ from sqlalchemy.orm import Session
 from core.config import settings
 
 _fake = faker.Faker()
-
-
-def test_get_categories_list(
-    client: TestClient, normal_user_token_headers: Dict[str, str], db: Session
-) -> None:
-    r = client.get(
-        f"{settings.API_V1_STR}/categories"
-        # headers=normal_user_token_headers
-    )
-
-    assert r.status_code == 200
-    rjson = r.json()
-    assert rjson
-    assert isinstance(rjson, list)
-    assert rjson[0]["name"]
-    assert rjson[0]["id"]
-
-
-def test_get_categories(
-    client: TestClient,
-) -> None:
-    categories_id = 1
-    r = client.get(
-        f"{settings.API_V1_STR}/categories/{categories_id}"
-        # headers=normal_user_token_headers
-    )
-
-    assert r.status_code == 200
-    rjson = r.json()
-    assert rjson
-    assert rjson["id"] == categories_id
-
-
-def test_get_quote_category_by_name(
-    client: TestClient,
-) -> None:
-    category_name = "James King"
-    r = client.get(
-        f"{settings.API_V1_STR}/categories/name/{category_name}"
-        # headers=normal_user_token_headers
-    )
-
-    assert r.status_code == 200
-    rjson = r.json()
-    assert rjson
-    assert rjson["name"] == category_name
-
-
-def test_get_quote_category_by_parent_id(
-    client: TestClient,
-) -> None:
-    parent_id = 15
-    r = client.get(
-        f"{settings.API_V1_STR}/categories/parent/{parent_id}"
-        # headers=normal_user_token_headers
-    )
-
-    assert r.status_code == 200
-    rjson = r.json()
-    assert rjson
-    assert type(rjson) == list
-    assert rjson[0]["parent_id"] == parent_id
 
 
 def test_create_categories(
@@ -101,6 +37,101 @@ def test_update_categories(
     assert content["name"] == data["name"]
     assert "id" in content
     assert content["id"] == categories_id
+
+
+def test_get_categories_list(
+    client: TestClient, superuser_token_headers: dict, db: Session
+) -> None:
+    r = client.get(
+        f"{settings.API_V1_STR}/categories"
+        # headers=normal_user_token_headers
+    )
+
+    assert r.status_code == 200
+    rjson = r.json()
+    assert rjson
+    assert isinstance(rjson, list)
+    assert rjson[0]["name"]
+    assert rjson[0]["id"]
+
+
+def test_get_categories(
+    client: TestClient,
+) -> None:
+    categories_id = 1
+    r = client.get(
+        f"{settings.API_V1_STR}/categories/{categories_id}"
+        # headers=normal_user_token_headers
+    )
+
+    assert r.status_code == 200
+    rjson = r.json()
+    assert rjson
+    assert rjson["id"] == categories_id
+
+
+def test_get_quote_category_by_name(
+    client: TestClient,
+) -> None:
+    categories_id = 1
+    r = client.get(
+        f"{settings.API_V1_STR}/categories/{categories_id}"
+        # headers=normal_user_token_headers
+    )
+
+    assert r.status_code == 200
+    rjson = r.json()
+    assert rjson
+    assert rjson["id"] == categories_id
+
+    category_name = rjson["name"]
+    r = client.get(
+        f"{settings.API_V1_STR}/categories/name/{category_name}"
+        # headers=normal_user_token_headers
+    )
+
+    assert r.status_code == 200
+    rjson = r.json()
+    assert rjson
+    assert rjson["name"] == category_name
+
+
+def test_get_quote_category_by_parent_id(
+    client: TestClient, superuser_token_headers: dict, db: Session
+) -> None:
+    data1 = {"name": _fake.name()}
+    response = client.post(
+        f"{settings.API_V1_STR}/categories",
+        headers=superuser_token_headers,
+        json=data1,
+    )
+    assert response.status_code == 200
+    content1 = response.json()
+    assert content1["name"] == data1["name"]
+    assert "id" in content1
+
+    data2 = {"name": _fake.name(), "parent_id": content1["id"]}
+    response = client.post(
+        f"{settings.API_V1_STR}/categories",
+        headers=superuser_token_headers,
+        json=data2,
+    )
+    assert response.status_code == 200
+    content2 = response.json()
+    assert content2["name"] == data2["name"]
+    assert "id" in content2
+
+    parent_id = content1["id"]
+    r = client.get(
+        f"{settings.API_V1_STR}/categories/parent/{parent_id}"
+        # headers=normal_user_token_headers
+    )
+
+    assert r.status_code == 200
+    rjson = r.json()
+    assert rjson
+    assert type(rjson) == list
+    assert rjson[0]["parent_id"] == parent_id
 
 
 def test_delete_categories(
